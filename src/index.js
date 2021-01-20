59a532cd682df98b81d8e5004c6841d5fea83e40
handsomemolly <ryan.kendig@gmail.com>
2021-01-19 20:31:32 -0800

    close enough

--- src/index.js
+++ src/index.js
@@ -1,90 +1,13 @@
 let addToy = false;
-let toys;
-const toyContainer = document.getElementById('toy-collection')
-let newToy;
 
-
-function fetchToys() {
-  return fetch(`http://localhost:3000/toys`)
-  .then(res => res.json())
-  .then(json => storeToys(json))
-}
-
-function storeToys(json) {
-  toys = json;
-}
-
-function x(toy) {
-  let div = document.createElement('div')
-  let h2 = document.createElement('h2')
-  let img = document.createElement('img')
-  let p = document.createElement('p')
-  let btn = document.createElement('button')
-  h2.innerText = toy.name
-  img.src = toy.image
-  p.innerText = toy.likes
-  btn.classList.add('like-btn')
-  div.appendChild(h2)
-  div.appendChild(img)
-  div.appendChild(p)
-  div.appendChild(btn)
-  toyContainer.appendChild(div)
-}
-
-function renderToys() {
-  toys.forEach(toy => {
-    x(toy)
-  })
-}
-
-function renderNewToy() {
-  x(newToy)
-}
-
-
-function handleNewToy(e) {
-  e.preventDefault()
-  let valArray = document.getElementsByClassName('input-text')
-  let name = valArray[0].value 
-  let image = valArray[1].value
-  let temp = {
-    name: name,
-    image: image,
-    likes: 0
-  }
-  newToy = temp
-}
-
-
-function postToy() {
-  
-    let data = {
-        name: newToy.name,
-        image: newToy.image,
-        likes: newToy.likes
-    };
-    let obj = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(data)
-    };
-    fetch("http://localhost:3000/toys", obj)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(object) {
-        x(object);
-    })
-}
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  const newBtn = document.getElementsByClassName("submit")[0]
+  const toyForm = document.querySelector('form')
+
+  toyForm.addEventListener('submit', handleSubmit)
+
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -95,12 +18,80 @@ function postToy() {
       toyFormContainer.style.display = "none";
     }
   });
+});
 
-  newBtn.addEventListener("click", () => {
-      handleNewToy()
-      postToy()
-      console.log('hey')
-  });
+//DATA
+function fetchToys() {
+  fetch(`http://localhost:3000/toys`)
+  .then(res => res.json())
+  .then(toys => toys.forEach(toy => buildCard(toy)))
+}
 
-  fetchToys()
+function postToys(toy) {
+  fetch(`http://localhost:3000/toys`, {
+    method: 'POST',
+    headers: {
+        "Content Type": "application/json",
+        Accept: "application/json"
+    },
+      body: JSON.stringify(toy) 
+  })
+  .then(res => res.json())
+  .then(toy => buildCard(toy))
+}
 
+function updateToy(toy) {
+  toy.likes++ 
+
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method:'PATCH',
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({likes: toy.likes})
+  })
+  .then(res => res.json())
+  .then(toy => {
+    let oldToy = document.getElementById(toy.id)
+    let p = oldToy.querySelector('p')
+    p.textContent = `${toy.likes} Likes`
+  })
+}
+
+
+//DOM
+function buildCard(toy) {
+  let div = document.createElement('div')
+  let h2 = document.createElement('h2')
+  let img = document.createElement('img')
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+
+  let toyCollection = document.querySelector('#toy-collection')
+
+  div.className = 'card'
+  h2.textContent = toy.name
+  img.src = toy.image
+  img.className = 'toy-avatar'
+  p.textContent = `${toy.likes} likes`
+  btn.className = 'like-btn'
+  btn.textContent = 'Like <3'
+
+  btn.addEventListener('click', () => updateToy(toy))
+
+  div.append(h2, img, p, btn)
+  toyCollection.appendChild(div)
+
+}
+
+//Handle
+function handleSubmit(e) {
+  e.preventDefault()
+  let toy = {
+    name: e.target.name.value, //grab the name *value* attribute from the input on HTML
+    image: e.target.image.value, 
+    likes: 0
+  }
+  postToys(toy) 
+}
